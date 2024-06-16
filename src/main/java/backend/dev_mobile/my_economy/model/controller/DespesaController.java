6e473acae76e7585400263538997ea32dd20d4fe
Deivid Alves Cardoso <deividalves.cardoso@gmail.com>
2024-06-16 20:49:49 -0300

    refactor: Update DespesaController to include endpoint for summing expenses by month and user

--- src/main/java/backend/dev_mobile/my_economy/model/controller/DespesaController.java
+++ src/main/java/backend/dev_mobile/my_economy/model/controller/DespesaController.java
@@ -29,9 +29,7 @@ public class DespesaController {
     @GetMapping("/por-mes-e-login/{referenciaMes}/{login}")
     public List<Despesa> getByReferenciaMesAndUsuarioEmail(@PathVariable("referenciaMes") LocalDate referenciaMes,
             @PathVariable("login") String usuarioEmail) {
-                System.out.println("Sem alterar: "+referenciaMes);
                 referenciaMes = referenciaMes.withDayOfMonth(1);
-                System.out.println("Alterado: "+referenciaMes);
         return despesaService.getByReferenciaMesAndUsuarioEmail(referenciaMes, usuarioEmail);
     }
 
@@ -42,9 +40,7 @@ public class DespesaController {
 
     @PostMapping("/salvar")
     public Despesa salvar(@RequestBody Despesa despesa) {
-        System.out.println(despesa.getReferenciaMes());
         despesa.setReferenciaMes(despesa.getReferenciaMes().withDayOfMonth(1));
-        System.out.println(despesa.getReferenciaMes());
         return despesaService.salvar(despesa);
     }
 
@@ -64,9 +60,9 @@ public class DespesaController {
         return despesaService.atualizar(id, despesa);
     }
 
-    @GetMapping("/soma-por-mes/{referenciaMes}")
-    public double somarDespesasPorMes(@PathVariable("referenciaMes") LocalDate referenciaMes) {
-        List<Despesa> despesas = despesaService.getDespesasPorMes(referenciaMes);
+    @GetMapping("/soma-por-mes/{referenciaMes}/{usuarioEmail}")
+    public double somarDespesasPorMesEUsuario(@PathVariable("referenciaMes") LocalDate referenciaMes, @PathVariable("usuarioEmail") String usuarioEmail) {
+        List<Despesa> despesas = despesaService.getByReferenciaMesAndUsuarioEmail(referenciaMes, usuarioEmail);
         double total = 0.0;
         for (Despesa despesa : despesas) {
             total += despesa.getGasto();
